6725c68f9fc1cd502495394fda6f1c0e60fd4259
irelephant <irelephant@disroot.org>
2025-12-01 19:35:54 +0000

    fix css slightly on mobile, add template for instance info

--- config.lua
+++ config.lua
@@ -1,20 +1,14 @@
 local config = require("lapis.config")
 
 
-local instance_about = [[
-  The operator of this instance has not written an about message yet.
-]]
-
 config("development", {
   server = "nginx",
   code_cache = "off",
   num_workers = "1",
-  instance_about = instance_about
 })
 
 config("production", {
   port = "80",
   code_cache = "on",
   num_workers = "4",
-  instance_about = instance_about
 })
