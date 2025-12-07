57f665c0c06c568bae3b2b5e35e350ba9ac24514
irelephant <irelephant@disroot.org>
2025-12-07 18:24:24 +0000

    fix proxy.lua file.

--- conf/proxies.lua
+++ conf/proxies.lua
@@ -2,13 +2,13 @@
 -- note: use 127.0.0.1 instead of localhost.
 -- This only supports http proxies, not SOCKS.
 
-local alse = "http://127.0.0.1:9500"
+
 local proxies = {
-    posts = alse, -- Posts/reels, anything with a shortcode.
-    user_info = alse,
-    search = alse,
-    user_posts = alse,
-    comments = alse,
+    posts = false, -- Posts/reels, anything with a shortcode.
+    user_info = false,
+    search = false,
+    user_posts = false,
+    comments = false,
 }
 
 return proxies
