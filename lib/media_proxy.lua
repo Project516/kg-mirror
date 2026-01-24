943fa67792c5b6c130a98851e6596fd3a2a89a69
patrick <gh@patriick.dev>
2026-01-24 19:15:46 +0100

    Allow mediaproxy to stream media

--- lib/media_proxy.lua
+++ lib/media_proxy.lua
@@ -11,13 +11,14 @@ local WHITELIST = {
 
 app:get("/mediaproxy", function(self)
     local image_uri = util.unescape(self.params.url)
+    local httpc = http.new()
+    local parsed_uri = httpc:parse_uri(image_uri)
 
-    local match = ngx.re.match(image_uri, [[^(https?)://([^/]+)]], "jo")
-    if not match then
-        return { status = 400, "Invalid URL" }
+    if not parsed_uri then
+      return { status = 400, "Invalid URL" }
     end
 
-    local host = match[2]
+    local scheme, host, port, path = unpack(parsed_uri)
 
     local allowed = false
     for _, domain in ipairs(WHITELIST) do
@@ -31,18 +32,49 @@ app:get("/mediaproxy", function(self)
         return { status = 403, "Domain Not Allowed" }
     end
 
-    local httpc = http.new()
-    local res, err = httpc:request_uri(image_uri, { method = "GET" })
+    local ok, err, ssl_session = httpc:connect({
+      scheme = scheme,
+      host = host,
+      port = 443,
+    })
+
+    if err then
+      ngx.log(ngx.ERR, "[mediaproxy] Connecting failed: ", err)
+      return { status = 502, "Request Failed" }
+    end
+
+    local res, err = httpc:request({
+      path = path,
+      headers = {
+        ["Host"] = host,
+        ["Range"] = self.req.headers["Range"]
+      }
+    })
 
     if not res then
         ngx.log(ngx.ERR, "[mediaproxy] Request Failed: ", err)
         return { status = 502, "Request Failed" }
     end
 
+    ngx.status = res.status
+    ngx.header.content_type = res.headers["Content-Type"]
+    ngx.header.content_length = res.headers["Content-Length"]
+    ngx.header.content_range = res.headers["Content-Range"]
+
+    repeat
+      local buffer, err = res.body_reader(8192)
+      if err then
+        ngx.log(ngx.ERR, err)
+        break
+      end
+
+      if buffer then
+        ngx.print(buffer)
+      end
+    until not buffer
+
     return {
-        res.body,
-        layout = false,
-        headers = { ["Content-Type"] = res.headers["Content-Type"] }
+        skip_render = true
     }
 end)
 
